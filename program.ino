6a2969c0d203d8995d4a14aae6c09ce6b78f818f
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2019-01-17 15:14:09 +0100

    Create program.ino

---
+++ program.ino
@@ -0,0 +1,12 @@
+#include <Adafruit_VL53L0X.h>
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+void setup() {
+  // put your setup code here, to run once:
+ lox.begin(0x31);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
